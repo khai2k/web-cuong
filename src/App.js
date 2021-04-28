599a6cd9628a767068f771e174f6de1022fc3a32
Khai <ngovankhai2000@gmail.com>
2021-04-28 11:00:42 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
+import Main from './Main';
+import logo from "./logo.svg";
 function App() {
+  const [isHidden, setisHidden] = useState(false)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <div style={{ display: isHidden ? 'none' : 'block' }}>
+        <img src={logo} width="500" height="600" ></img>
+
+      </div>
+      <button onClick={() => { setisHidden(!isHidden) }}>fullscreen</button> */}
+      <Main />
+    </>
   );
 }
 
---
+++ src/Main.js
@@ -0,0 +1,38 @@
+import './App.css';
+import { useState } from 'react';
+
+function Main() {
+    const [rfId, setrfId] = useState('')
+    return (
+        <>
+            <input autoFocus className="input-hidden"
+                value={rfId} onChange={(e) => {
+                    setrfId(e.target.value);
+                    console.log(e.target.value)
+                }}></input>
+            <div className="header">
+                <div className="container-left">
+                    <div className="info">
+                        <div>Le Pham Huy Cuong</div>
+                        <div>70D1-25710</div>
+                    </div>
+                </div>
+                <div className="container-right">
+                    <div className="payment">
+                        <div>Tinh tien</div>
+                        <div>30000 VND</div>
+                    </div>
+                </div>
+            </div>
+            <div className="grid-container">
+                <div className="camera">1</div>
+                <div className="camera">2</div>
+                <div className="image">3</div>
+                <div className="image">4</div>
+            </div>
+
+        </>
+    );
+}
+
+export default Main;
